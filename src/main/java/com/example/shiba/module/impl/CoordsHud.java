a3a0158a0b49deb02aff1df6cb7215107e0fc3e0
Huynhaae <137372530+Huynhaae@users.noreply.github.com>
2026-07-29 17:11:51 +0700

    Create CoordsHud.java

---
+++ src/main/java/com/example/shiba/module/impl/CoordsHud.java
@@ -0,0 +1,7 @@
+package com.example.shiba.module.impl;
+
+import com.example.shiba.module.Module;
+
+public class CoordsHud extends Module {
+    public CoordsHud() { super("Coords", "Hien thi toa do tren HUD"); }
+}
