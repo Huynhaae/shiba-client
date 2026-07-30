cefcc3827baf55281fe192705c6232588341aa35
huynhaae <your-github-email@example.com>
2026-07-30 15:17:54 +0700

    Fix getByName method call in MixinEntity

--- src/main/java/com/example/shiba/mixin/MixinEntity.java
+++ src/main/java/com/example/shiba/mixin/MixinEntity.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinEntity {
     @Inject(method = "getTargetingMargin", at = @At("HEAD"), cancellable = true)
     private void onGetTargetingMargin(CallbackInfoReturnable<Float> cir) {
-        Hitbox hitbox = (Hitbox) ModuleManager.getModuleByName("Hitbox");
+        Hitbox hitbox = (Hitbox) ModuleManager.getByName("Hitbox");
         if (hitbox != null && hitbox.isEnabled()) {
             cir.setReturnValue(Hitbox.expand);
         }
