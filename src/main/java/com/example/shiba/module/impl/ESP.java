b665f3661685c97bf66f28fba993d1189342ca02
Huynhaae <137372530+Huynhaae@users.noreply.github.com>
2026-07-31 13:33:01 +0700

    Update ESP.java

--- src/main/java/com/example/shiba/module/impl/ESP.java
+++ src/main/java/com/example/shiba/module/impl/ESP.java
@@ -4,7 +4,7 @@ import com.example.shiba.module.Category;
 import com.example.shiba.module.Module;
 
 public class ESP extends Module {
-    public double range = 40.0;
+    public double range = 5000.0;
 
     public ESP() {
         super("ESP", "Hien khung entity xuyen tuong.", Category.RENDER);
