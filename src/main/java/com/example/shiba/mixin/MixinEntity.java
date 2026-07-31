21dc2b1045acbfaf356cf3281fcadcd1c453ee14
Huynhaae <137372530+Huynhaae@users.noreply.github.com>
2026-07-31 13:08:47 +0700

    Update MixinEntity.java

--- src/main/java/com/example/shiba/mixin/MixinEntity.java
+++ src/main/java/com/example/shiba/mixin/MixinEntity.java
@@ -5,7 +5,6 @@ import com.example.shiba.module.impl.Hitbox;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Entity.class)
 public class MixinEntity {
 
-    @Inject(method = "getBoundingBox", at = @At("RETURN"), cancellable = true)
-    private void shiba$expandBoundingBox(CallbackInfoReturnable<Box> cir) {
+    @Inject(method = "getTargetingMargin", at = @At("RETURN"), cancellable = true)
+    private void shiba$expandTargetingMargin(CallbackInfoReturnable<Float> cir) {
         Entity self = (Entity) (Object) this;
 
         if (!(self instanceof LivingEntity)) return;
@@ -26,6 +25,6 @@ public class MixinEntity {
         Hitbox hitbox = ModuleManager.HITBOX;
         if (hitbox == null || !hitbox.isEnabled()) return;
 
-        cir.setReturnValue(cir.getReturnValue().expand(hitbox.expand));
+        cir.setReturnValue((float) hitbox.expand);
     }
 }
