73032d6250fdc3df00b1371efc9b3c158bffaf9f
Huynhaae <137372530+Huynhaae@users.noreply.github.com>
2026-07-31 09:09:30 +0700

    Add ESP module for rendering entities

---
+++ src/main/java/com/example/shiba/module/impl/ESP.java
@@ -0,0 +1,12 @@
+package com.example.shiba.module.impl;
+
+import com.example.shiba.module.Category;
+import com.example.shiba.module.Module;
+
+public class ESP extends Module {
+    public double range = 40.0;
+
+    public ESP() {
+        super("ESP", "Hien khung entity xuyen tuong.", Category.RENDER);
+    }
+}
