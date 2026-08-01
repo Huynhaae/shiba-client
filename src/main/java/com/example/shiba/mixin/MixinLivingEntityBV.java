6bc42094ea677a5d01b900aa8c9b269e31a5d8a5
shiba <137372530+Shineverthinkforit019@users.noreply.github.com>
2026-08-01 12:24:34 +0700

    Update MixinLivingEntityBV.java

--- src/main/java/com/example/shiba/mixin/MixinLivingEntityBV.java
+++ src/main/java/com/example/shiba/mixin/MixinLivingEntityBV.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LivingEntity.class)
+@Mixin(Entity.class)
 public class MixinLivingEntityBV {
 
     @Inject(method = "getBoundingBox", at = @At("RETURN"), cancellable = true)
